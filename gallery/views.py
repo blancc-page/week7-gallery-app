3677ab4bf3a070e778e20d8e0460543434fd3645
blancc-page <blancc.666@gmail.com>
2022-06-07 11:23:52 +0300

    feat[gallery]: create app called gallery

---
+++ gallery/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+from .models import Image,Location,Category
+
+# Create your views here.
+
+def home(request):
+    images = Image.get_all_images()
+    length = len(images)
+    locations = Location.objects.all()
+
+    context ={
+        "images":images,
+        "locations":locations,
+        "length":length
+    }
+
+    return render(request,'gallery/index.html',context)
+
+def search(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        query = request.GET.get("category")
+        res = Image.search_by_category(query)
+        length = len(res)
+        message = f'Search results for {query}'
+        locations = Location.objects.all()
+        
+        
+        context = {
+            'images': res,
+            "message":message,
+            "length":length,
+            "query":query,
+            "locations":locations
+        }
+
+        return render(request,'gallery/search.html',context)
+
+    else:
+        message = 'You have not searched for any item'
+        return render(request,'gallery/search.html',message)
+def filter_location(request):
+    if 'location' in request.GET and request.GET["location"]:
+        query = request.GET.get("location")
+        res = Image.filter_by_location(query)
+        locations = Location.objects.all()
+        length = len(res)
+        message = f'{query}'
+
+        context = {
+            'images': res,
+            "message":message,
+            "length":length,
+            "locations":locations,
+            "query":query
+        }
+
+        
+        
+        return render(request,'gallery/location.html',context)
+
+    else:
+        return render(request,'gallery/location.html',context)
+
+   
+
